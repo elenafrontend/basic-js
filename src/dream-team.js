b6fb716b399ea9fedfbfd644955089f085f2621d
Elena Gumerova <elenafrontend@ya.ru>
2021-10-04 21:09:37 +0300

    feat: implement dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,60 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  let teamName = '';
+
+  try {
+    members.forEach(member => {
+      if(typeof member === 'string' && !!member) {
+        
+        for (let i = 0; i < member.length; i++) {
+          if (member[i] !== ' ') {
+            teamName += member[i].toUpperCase();
+            break
+          }
+        }
+      }
+    })
+  } catch (error) {
+    return false
+  }
+
+  if (!!teamName) {
+    
+    teamName = teamName.split('');
+    
+    for (let i = 0; i < teamName.length - 1; i++) {
+      let firstLetter = teamName[i];
+  
+      for(let j = i + 1; j < teamName.length; j++) {
+        if (firstLetter > teamName[j]) {
+          firstLetter = teamName[j];
+          let swap = teamName[i];
+          teamName[i] = firstLetter;
+          teamName[j] = swap;
+        } 
+      }
+    }
+    return teamName.join('')
+  } else {
+    return false
+  }
 }
+
+createDreamTeam(
+  [
+    '   William Alston ',
+    ' Paul Benacerraf',
+    '  Ross Cameron',
+    '       Gilles Deleuze',
+    '  Arda Denkel ',
+    '  Michael Devitt',
+    '  Kit Fine',
+    ' Nelson Goodman',
+    'David Kolb',
+    '   Saul Kripke',
+    '  Trenton Merricks',
+    '  Jay Rosenberg',
+  ]
+)
